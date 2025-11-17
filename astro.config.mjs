78a82ec86fe69eb373f0974c75763d656e6de4ed
trmuba2502 <hanhtrangvictoria@gmail.com>
2025-11-17 10:06:28 +0700

    Setup before build

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://minhi1.github.io",
-	base: "/minhi1-blogs",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
